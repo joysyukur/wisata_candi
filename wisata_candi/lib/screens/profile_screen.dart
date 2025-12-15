2a91bb2c184827b71918c8d1b2e1edc856df2cea
joysyukur <joysyukurtatemaberkatdohona_2327240112@mhs.mdp.ac.id>
2025-12-15 12:36:37 +0700

    update

--- wisata_candi/lib/screens/profile_screen.dart
+++ wisata_candi/lib/screens/profile_screen.dart
@@ -1,5 +1,6 @@
+import 'package:encrypt/encrypt.dart' as encrypt;
 import 'package:flutter/material.dart';
-import 'package:wisata_candi/widgets/profile_file_item.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 class ProfileScreen extends StatefulWidget {
   const ProfileScreen({super.key});
@@ -9,24 +10,69 @@ class ProfileScreen extends StatefulWidget {
 }
 
 class _ProfileScreenState extends State<ProfileScreen> {
-  //   TODO 1 : Deklarasi variabel yang dibutuhkan
+  // 1. Declare necessary variables
   bool isSignedIn = false;
-  String fullName = '';
-  String userName = '';
+  String fullName = ''; // Example name
+  String userName = ""; // Example username
   int favoriteCandiCount = 0;
+  late Color iconColor;
 
-  // TODO 5. Implementasi fungsi signIn
+  //5. implementasi fungsi signIn
   void signIn() {
+    Navigator.pushNamed(context, "/signin");
+    // setState(() {
+    //   isSignedIn = !isSignedIn;
+    //   userName = 'faiz';
+    //   fullName = 'Faiz Ganteng';
+    //   favoriteCandiCount = 3;
+    // });
+  }
+
+  //6. implementasi fungsi signOut
+  void signOut() async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    await prefs.setBool('isSignedIn', false);
+
+    //await prefs.remove('username');
+    //await prefs.remove('name');
+
     setState(() {
       isSignedIn = !isSignedIn;
+      userName = '';
+      fullName = '';
     });
   }
 
-  // TODO 6. Implementasi fungsi signOut
-  void signOut() {
+  void _checkSignInStatus() async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
     setState(() {
-      isSignedIn = !isSignedIn;
+      isSignedIn = prefs.getBool("isSignedIn") ?? false;
     });
+  }
+
+  void _identitas() async {
+
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    setState(() {
+      fullName = prefs.getString("fullname") ?? "";
+      userName = prefs.getString("username") ?? "";
+    });
+    final keyString = prefs.getString('key') ?? '';
+    final ivString = prefs.getString('iv') ?? '';
+    final encrypt.Key key = encrypt.Key.fromBase64(keyString);
+    final iv = encrypt.IV.fromBase64(ivString);
+
+    final encrypter = encrypt.Encrypter(encrypt.AES(key));
+    fullName = encrypter.decrypt64(fullName, iv: iv);
+    userName = encrypter.decrypt64(userName, iv: iv);
+  }
+
+  @override
+  void initState() {
+    _checkSignInStatus();
+    _identitas();
+
+    super.initState();
   }
 
   @override
@@ -43,11 +89,10 @@ class _ProfileScreenState extends State<ProfileScreen> {
             padding: const EdgeInsets.symmetric(horizontal: 16),
             child: Column(
               children: [
-                //   TODO 2 : Buat bagian ProfileHeader yang berisi gambar profile
                 Align(
                   alignment: Alignment.topCenter,
                   child: Padding(
-                    padding: const EdgeInsets.only(top: 200 - 50),
+                    padding: const EdgeInsets.only(top: 150), // 200 - 50 = 150
                     child: Stack(
                       alignment: Alignment.bottomRight,
                       children: [
@@ -78,44 +123,93 @@ class _ProfileScreenState extends State<ProfileScreen> {
                     ),
                   ),
                 ),
-                //   TODO 3 : Buat bagian ProfileInfor yang berisi info profile
                 SizedBox(height: 20),
                 Divider(color: Colors.deepPurple[100]),
                 SizedBox(height: 4),
-                ProfileInfoItem(
-                  icon: Icons.lock,
-                  iconColor: Colors.amber,
-                  label: 'Pengguna',
-                  value: fullName,
-                  showEditIcon: isSignedIn,
-                  onEditPressed: () {},
+                Row(
+                  children: [
+                    SizedBox(
+                      width: MediaQuery.of(context).size.width / 3,
+                      child: Row(
+                        children: [
+                          Icon(Icons.lock, color: Colors.amber),
+                          SizedBox(width: 8),
+                          Text(
+                            'Pengguna',
+                            style: TextStyle(
+                              fontSize: 18,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                    Expanded(
+                      child: Text(
+                        ': $userName',
+                        style: TextStyle(fontSize: 18),
+                      ),
+                    ),
+                  ],
                 ),
                 SizedBox(height: 4),
                 Divider(color: Colors.deepPurple[100]),
                 SizedBox(height: 4),
-                ProfileInfoItem(
-                  icon: Icons.person,
-                  label: 'Nama',
-                  value: userName,
-                  showEditIcon: isSignedIn,
-                  onEditPressed: () {
-                    debugPrint('Icon edit ditekan ...');
-                  },
-                  iconColor: Colors.blue,
+                Row(
+                  children: [
+                    SizedBox(
+                      width: MediaQuery.of(context).size.width / 3,
+                      child: Row(
+                        children: [
+                          Icon(Icons.person, color: Colors.blue),
+                          SizedBox(width: 8),
+                          Text(
+                            'Nama',
+                            style: TextStyle(
+                              fontSize: 18,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                    Expanded(
+                      child: Text(
+                        ': $fullName',
+                        style: TextStyle(fontSize: 18),
+                      ),
+                    ),
+                  ],
                 ),
                 SizedBox(height: 4),
                 Divider(color: Colors.deepPurple[100]),
                 SizedBox(height: 4),
-                ProfileInfoItem(
-                  icon: Icons.favorite,
-                  label: 'Favorit',
-                  value: favoriteCandiCount > 0 ? '$favoriteCandiCount' : '',
-                  iconColor: Colors.red,
+                Row(
+                  children: [
+                    SizedBox(
+                      width: MediaQuery.of(context).size.width / 3,
+                      child: Row(
+                        children: [
+                          Icon(Icons.favorite, color: Colors.red),
+                          SizedBox(width: 8),
+                          Text(
+                            'Favorite',
+                            style: TextStyle(
+                              fontSize: 18,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                    Expanded(
+                      child: Text(
+                        ': $favoriteCandiCount',
+                        style: TextStyle(fontSize: 18),
+                      ),
+                    ),
+                  ],
                 ),
-                //   TODO 4 : Buat bagian ProfileActions yang berisi TextButton sign in/out
-                SizedBox(height: 4),
-                Divider(color: Colors.deepPurple[100]),
-                SizedBox(height: 4),
                 isSignedIn
                     ? TextButton(onPressed: signOut, child: Text('Sign Out'))
                     : TextButton(onPressed: signIn, child: Text('Sign In')),
--- wisata_candi/lib/screens/search_screen.dart
+++ wisata_candi/lib/screens/search_screen.dart
@@ -10,103 +10,88 @@ class SearchScreen extends StatefulWidget {
 }
 
 class _SearchScreenState extends State<SearchScreen> {
-//   TODO: 1. Deklarasi variabel yang dibutuhkan
-
+  //TODO 1 Variabel
   List<Candi> _filteredCandis = candiList;
-  String _searchQuery = '';
+  String _searchQuery = "";
   final TextEditingController _searchController = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-    //   TODO: 2. Buat appbar dengan judul pencarian candi
-      appBar: AppBar(title: Text('Pencarian Candi'),),
-    //   TODO: 3. Buat body berupa column
+      //TODO 2 APPBar
+      appBar: AppBar(
+        title: Text('Pencarian Candi'),
+      ),
+      //TODO 3 Body
       body: Column(
         children: [
-          //   TODO: 4. Buat TextField pencarian sebagai anak dari column
+          //TODO 4 Textfield Search
           Padding(
             padding: EdgeInsets.all(16),
             child: Container(
               decoration: BoxDecoration(
-                color: Colors.deepPurple[50],
-                borderRadius: BorderRadius.circular(8),
-              ),
+                  borderRadius: BorderRadius.circular(5),
+                  color: Colors.deepPurple[50]),
               child: TextField(
-                controller: _searchController,
-                onChanged: (value) {
-                  setState(() {
-                    _searchQuery = value;
-                  });
-                },
-                // TODO: 6. Implementasi fitur pencarian
+                autofocus: false,
                 decoration: InputDecoration(
-                  hintText: 'Cari candi...',
-                  prefixIcon: Icon(
-                    Icons.search,
-                    color: Colors.deepPurple
-                  ),
-                  // TODO 7. Implementasi pengosongan input
-                  border: InputBorder.none,
-                  contentPadding:
-                    EdgeInsets.symmetric(horizontal: 16, vertical: 12),
-                ),
+                    hintText: "Cari Candi....",
+                    prefixIcon: Icon(Icons.search),
+                    border: InputBorder.none,
+                    focusedBorder: OutlineInputBorder(
+                        borderSide: BorderSide(color: Colors.deepPurple)),
+                    contentPadding:
+                    EdgeInsets.symmetric(horizontal: 16, vertical: 12)),
               ),
             ),
           ),
-          //   TODO: 5. Buat ListView hasil pencarian sebagai anak dari Column
+          //TODO 5 Listview
           Expanded(
             child: ListView.builder(
-              itemCount: _filteredCandis.length,
-              itemBuilder: (context, index) {
-                final candi = _filteredCandis[index];
-                // TODO: 8. Implementasi GestureDetector dan Hero animation
-                return Card(
-                  margin: EdgeInsets.symmetric(horizontal: 16, vertical: 4),
-                  child: Row(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Container(
-                        padding: EdgeInsets.all(8),
-                        width: 100,
-                        height: 100,
-                        child: ClipRRect(
-                          borderRadius: BorderRadius.circular(10),
-                          child: Image.asset(
-                            candi.imageAsset,
-                            fit: BoxFit.cover,
+                itemCount: _filteredCandis.length,
+                itemBuilder: (content, index) {
+                  final candi = _filteredCandis[index];
+                  return Card(
+                    margin: EdgeInsets.symmetric(horizontal: 16, vertical: 4),
+                    child: Row(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Container(
+                          padding: const EdgeInsets.all(8),
+                          width: 100,
+                          height: 100,
+                          child: ClipRRect(
+                            borderRadius: BorderRadius.circular(10),
+                            child: Image.asset(
+                              candi.imageAsset,
+                              fit: BoxFit.cover,
+                            ),
                           ),
                         ),
-                      ),
-                      Padding(
-                        padding: EdgeInsets.all(8),
-                        child: Column(
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          children: [
-                            Text(
-                              candi.name,
-                              style: TextStyle(
-                                fontSize: 16,
-                                fontWeight: FontWeight.bold,
+                        Padding(
+                          padding: const EdgeInsets.all(8.0),
+                          child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Text(
+                                candi.name,
+                                style: TextStyle(fontSize: 16),
                               ),
-                            ),
-                            SizedBox(height: 4),
-                            Text(
-                              candi.location,
-                              style: TextStyle(
-                                fontSize: 16,
-                                fontWeight: FontWeight.bold,
+                              SizedBox(
+                                height: 4,
                               ),
-                            ),
-                          ],
-                        ),
-                      ),
-                    ],
-                  ),
-                );
-              }
-            ),
+                              Text(candi.location)
+                            ],
+                          ),
+                        )
+                      ],
+                    ),
+                  );
+                }),
           ),
+          const SizedBox(
+            height: 16,
+          )
         ],
       ),
     );
